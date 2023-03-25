4e670c792a3f10683722734b34ea1110442577e3
estebangf <esteban.fernandez93@outlook.com.ar>
2023-03-24 21:35:40 -0300

    random light y correct signals

---
+++ Cave.gd
@@ -0,0 +1,36 @@
+extends Node2D
+
+@onready var Light_trap = preload("res://Light_trap.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	for n in 4:
+		create_lights_in_cuadrant(Vector2(
+			randi_range(-75,0),
+			randi_range(0,75)
+		))
+
+	for n in 4:
+		create_lights_in_cuadrant(Vector2(
+			randi_range(0,75),
+			randi_range(0,75)
+		))
+
+	for n in 4:
+		create_lights_in_cuadrant(Vector2(
+			randi_range(-75,0),
+			randi_range(75,150)
+		))
+
+	for n in 4:
+		create_lights_in_cuadrant(Vector2(
+			randi_range(0,75),
+			randi_range(75,150)
+		))
+
+
+func create_lights_in_cuadrant(cuadrant):
+	var random_position = cuadrant
+	var light_trap = Light_trap.instantiate()
+	light_trap.position = random_position
+	self.add_child(light_trap)
--- Light_trap.gd
+++ Light_trap.gd
@@ -9,33 +9,17 @@ enum NPC_state { EMERGED, HIDED, HIDING, EATING }
 
 var current_state : NPC_state = NPC_state.EMERGED
 
-func _ready():
-	var cerato_area = get_node("../CeratoPlayer/Area2D")
-	var lightrap_area = get_node("Area2D")
-	
-	cerato_area.area_entered.connect(_on_area_2d_area_entered)
-	cerato_area.area_exited.connect(_on_area_2d_area_exited)
+func _on_area_2d_body_entered(body):
+	if(body.name == "CeratoPlayer"):
+		if (current_state == NPC_state.EMERGED):
+			state_machine.travel("Hide")
+			current_state = NPC_state.HIDED
+		if (current_state == NPC_state.HIDED):
+			state_machine.travel("Idle_hide")
+	#	pass # Replace with function body.
 
-func _process(delta):
-	pass
-#	select_animation()
-
-#func hide():
-#	if ()
-
-#func select_animation():
-#	if (current_state == NPC_state.EMERGED):
-#		state_machine.travel("Idle_moving")
-
-func _on_area_2d_area_entered(area):
-	if (current_state == NPC_state.EMERGED):
-		state_machine.travel("Hide")
-		current_state = NPC_state.HIDED
-	if (current_state == NPC_state.HIDED):
-		state_machine.travel("Idle_hide")
-#	pass # Replace with function body.
-
-func _on_area_2d_area_exited(area):
-	if(current_state == NPC_state.HIDED):
-		state_machine.travel("Show_up")
-		current_state = NPC_state.EMERGED
+func _on_area_2d_body_exited(body):
+	if(body.name == "CeratoPlayer"):
+		if(current_state == NPC_state.HIDED):
+			state_machine.travel("Show_up")
+			current_state = NPC_state.EMERGED
